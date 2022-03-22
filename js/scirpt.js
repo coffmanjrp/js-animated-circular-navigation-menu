d60ecd8930bd72cb71d08f2c3ecfc356464e96eb
coffmanjrp <coffmanjrp@gmail.com>
2022-03-22 07:21:04 -0700

    Set menu indicator

--- js/scirpt.js
+++ js/scirpt.js
@@ -1,7 +1,19 @@
 const menu = document.querySelector('.menu');
 const menuToggle = document.querySelector('.toggle');
+const list = document.querySelectorAll('.list');
 
 menuToggle.addEventListener('click', () => {
   menu.classList.toggle('active');
   menuToggle.classList.toggle('active');
+});
+
+function activeLink() {
+  list.forEach((item) => {
+    item.classList.remove('active');
+  });
+  this.classList.add('active');
+}
+
+list.forEach((item) => {
+  item.addEventListener('mouseover', activeLink);
 });
